05738c0dedef1a31fcf0dcfc13945938d87109b5
Victor Zinoviev <viczinoviev@gmail.com>
2026-04-06 18:47:02 -0400

    feat: add /goodbye page

--- index.test.js
+++ index.test.js
@@ -18,3 +18,9 @@ test('GET /add/3/4 returns 7', async () => {
     expect(response.status).toBe(200);
     expect(response.body.result).toBe(7);
 });
+
+test ('GET /goodbye returns goodbye message', async () => {
+    const response = await request(app).get('/goodbye');
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Goodbye! See you next time.');
+});
